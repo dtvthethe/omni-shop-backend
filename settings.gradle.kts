4f4db528bab018f9d218152e882dbda0cc4ec14d
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2025-05-04 02:47:33 +0700

    check-struct

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "backend"
+rootProject.name = "omni-shop-backend"
---
+++ src/main/kotlin/com/omni/backend/controller/IndexController.kt
@@ -0,0 +1,14 @@
+package com.omni.backend.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("")
+class IndexController {
+    @GetMapping("")
+    fun index(): String {
+        return "Omni Shop API"
+    }
+}
